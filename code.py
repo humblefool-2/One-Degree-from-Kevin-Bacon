17185a14c7816406a725429a194acc42bf4cc4ee
ajain17012014 <ajain17012014@gmail.com>
2016-11-07 13:52:42 -0500

    Added functions

--- code.py
+++ code.py
@@ -17,13 +17,15 @@ def scrape_all_movies(url): #returns all movies' name and their urls
     response.append(dict)                                                                                                                                                                   
   return response                                                                                                                                                                           
                                                                                                                                                                                             
-def look_actor_name_by_id(actor_id): #returns actor'name by its id                                                                                                                          
+def look_actor_name_by_id(actor_id): #returns actor'name by its id   
+  actor_id=str(actor_id)
   response = requests.get("https://api.themoviedb.org/3/person/"+actor_id+"?api_key=e307d3989f772742e4794219effaf23f&language=en-US")                                                       
   #print (response.status_code)                                                                                                                                                             
   x=response.json()                                                                                                                                                                         
   print (x["name"])   
   
 def req_movies_for_actor(actor_id): #returns all the movies in which an actor with actor_id has been casted 
+  actor_id=str(actor_id)
   response = requests.get("https://api.themoviedb.org/3/person/"+actor_id+"?api_key=e307d3989f772742e4794219effaf23f&language=en-US")                                                                                                                                                                                                                   
   x=response.json() 
   url='http://www.imdb.com/name/'+x["imdb_id"]+'/'
@@ -44,13 +46,14 @@ def req_movies_for_actor(actor_id): #returns all the movies in which an actor wi
           "parent": actor_id
         }
       }
-      print (movie_dict)                                                                                                                                                                           
-    response.append(dict) 
-    time.sleep(2)
+      #print (movie_dict)                                                                                                                                                                           
+      response.append(movie_dict) 
+    #time.sleep(1)
   return response 
 
 def req_actors_for_movie(movie_id): #returns all the cast members in the movie with movie_id
   r=requests.get("https://api.themoviedb.org/3/movie/"+str(movie_id)+"?api_key=e307d3989f772742e4794219effaf23f&language=en-US")
+  #print (r.status_code)
   x=r.json()
   url='http://www.imdb.com/title/'+x["imdb_id"]+'/'
   page=urllib.request.urlopen(url)
@@ -66,13 +69,39 @@ def req_actors_for_movie(movie_id): #returns all the cast members in the movie w
                 "parent": movie_id
             }
     }
-    print (cast_dict)
-    response.append(cast_dict)
-      
+    #print (cast_dict)
+    response.append(cast_dict) 
+    
+  '''res2=[]
+  for j in response:
+    for v in j.values():
+      if v['name']!='Kevin Bacon':
+         res2.append(j)
+         i+=1'''
+        
   return response 
+
+def one_deg_from_actor(from_actor_id): #returns all the co-stars for an actor with from_actor_id
+  result=[]
+  name=look_actor_name_by_id(from_actor_id)
+  res1=req_movies_for_actor(from_actor_id)
+  res2=[]
+  result.append(res1)
+  for i in res1:
+    for k in i:
+      time.sleep(1)
+      response=req_actors_for_movie(k)
+      for j in response:
+        for v in j.values():
+          if name!=v['name']:
+            res2.append(j)
+  #print (res2)
+  result.append(res2)
+  return result
                                                                                                                                                                                             
 if __name__ == "__main__":                                                                                                                                                                  
-  #look_actor_name_by_id(str(4724))                                                                                                                                                          
+  #look_actor_name_by_id(4724)                                                                                                                                                          
   #scrape_all_movies("http://www.imdb.com/name/nm0000102/")
-  #req_movies_for_actor(str(4724))
-  req_actors_for_movie(916)
+  #req_movies_for_actor(4724)
+  #req_actors_for_movie(388399)
+  #one_deg_from_actor(4724)
